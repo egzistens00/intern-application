4ed2cbffa32956a0a089a0156d90a48c80f78a60
Alif Danial <alifdanial969@gmail.com>
2025-07-10 21:27:01 +0800

    a

--- Models/Intern.cs
+++ Models/Intern.cs
@@ -1,14 +1,10 @@
-﻿namespace InternAPI.Models
+﻿public class Intern
 {
-    public class Intern
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string Education { get; set; } = "";
-        public string Phone { get; set; } = "";
-        public string Gender { get; set; } = "";
-        public string Duration { get; set; } = "";
-        // Removed: public string ResumeFileName { get; set; } = "";
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Education { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Gender { get; set; } = "";
+    public string Duration { get; set; } = "";
 }
